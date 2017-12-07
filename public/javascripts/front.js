7ec8c4d68821d0868f2068e69b71a8fb9da85904
shiro83 <shirotori83@gmail.com>
2017-12-07 15:41:17 +0900

    a

--- public/javascripts/front.js
+++ public/javascripts/front.js
@@ -1,46 +1,45 @@
   $(function(){
     google.charts.load('current', {'packages':['corechart']});
     google.charts.setOnLoadCallback(drawChart);
-    setInterval(drawChart,1000);
+    setInterval(drawChart,2000);
 
-    // $('.vote_click').click(function(event) {
-    //   // HTMLでの送信をキャンセル
-    //   event.preventDefault();
-    //
-    //     // 操作対象のフォーム要素を取得
-    //     var val = $(this).val();
-    //
-    //     // 送信
-    //     $.ajax({
-    //         url: "/sum/vote",
-    //         async: false,
-    //         data: {
-    //               "vote": val
-    //         },
-    //         timeout: 10000,  // 単位はミリ秒
-    //
-    //         // 送信前
-    //         beforeSend: function(xhr, settings) {
-    //             // ボタンを無効化し、二重送信を防止
-    //             $('.vote_click').attr('disabled', true);
-    //         },
-    //         // 応答後
-    //         complete: function(xhr, textStatus) {
-    //             // ボタンを有効化し、再送信を許可
-    //             $('.vote_click').attr('disabled', false);
-    //         },
-    //
-    //         // // 通信成功時の処理
-    //         // success: function(result, textStatus, xhr) {
-    //         //     alert('OK');
-    //         // },
-    //         //
-    //         // // 通信失敗時の処理
-    //         // error: function(xhr, textStatus, error) {
-    //         //     alert('NG...');
-    //         // }
-    //     });
-    // });
+    $('.vote_click').click(function(event) {
+      // HTMLでの送信をキャンセル
+      event.preventDefault();
+
+        // 操作対象のフォーム要素を取得
+        var val = $(this).val();
+
+        // 送信
+        $.ajax({
+            url: "/sum/vote",
+            async: false,
+            data: {
+                  "vote": val
+            },
+            timeout: 10000,  // 単位はミリ秒
+
+            // 送信前
+            beforeSend: function(xhr, settings) {
+                // ボタンを無効化し、二重送信を防止
+                $('.vote_click').attr('disabled', true);
+            },
+            // 応答後
+            complete: function(xhr, textStatus) {
+                // ボタンを有効化し、再送信を許可
+                $('.vote_click').attr('disabled', false);
+            },
+
+            // 通信成功時の処理
+            success: function(result, textStatus, xhr) {
+            },
+
+            // 通信失敗時の処理
+            error: function(xhr, textStatus, error) {
+                alert('NG...');
+            }
+        });
+    });
   });
 
   function drawChart() {
--- routes/sum.js
+++ routes/sum.js
@@ -9,31 +9,31 @@ var pie_json =
   cols: [{label: 'Topping', type: 'string'},
          {label: 'Slices', type: 'number'}
   ],
-  rows: [{c:[{v: 'a'},{v: 1}]},
-         {c:[{v: 'b'},{v: 33}]},
-         {c:[{v: 'c'},{v: 22}]}
+  rows: [{c:[{v: 'berry_good'},{v: 0}]},
+         {c:[{v: 'good'},{v: 0}]},
+         {c:[{v: 'bad'},{v: 0}]},
+         {c:[{v: 'berry_bad'},{v: 0}]}
   ]
 };
 
 /* GET home page. */
 router.get('/pie', function(req, res, next) {
-
-  for(key1 in pie_json.rows) {
-    var c = pie_json.rows[key1];
-    for(key in c){
-      if (c[key][0].v == 'a') {
-        c[key][1].v += 1;
-        console.log(c[key][1].v);
-      }
-    }
-  }
-
+  res.json(pie_json);
 });
 
 
 router.get('/vote', function(req, res, next) {
-  console.log("vote");
-  // console.log(req.query.vote);
+  console.log(req.query.vote);
+  var vote_val = req.query.vote;
+  for(key1 in pie_json.rows) {
+    var c = pie_json.rows[key1];
+    for(key in c){
+      if (c[key][0].v == vote_val) {
+        c[key][1].v += 1;
+      }
+    }
+  }
+  res.send();
 });
 
 module.exports = router;
